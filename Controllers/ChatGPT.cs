ebd0ee60f0c0af4722407b5304071a9fe0885091
wilsonlo0130 <wilsonlo@cybersoft4u.com>
2022-12-12 11:51:40 +0800

    MacGyver Rabbit with Line Bot

--- Controllers/ChatGPT.cs
+++ Controllers/ChatGPT.cs
@@ -1,12 +1,6 @@
-using System;
 using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
 using System.Net.Http;
-using System.Web;
+using System.Text;
 
 namespace isRock.Template
 {
@@ -19,19 +13,13 @@ namespace isRock.Template
 
             // Request headers.
             client.DefaultRequestHeaders.Add(
-                "Authorization", "Bearer ________chatGPT_Token_______________");
+                "Authorization", "Bearer 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");
 
-            var JsonString = @"
-            {
-  ""model"": ""text-davinci-003"",
-  ""prompt"": ""question"",
-  ""max_tokens"": 4000,
-  ""temperature"": 0
-}
-            ".Replace("question", msg);
+            var JsonString = @"{""model"": ""text-davinci-003"", ""prompt"": ""question"", ""max_tokens"": 4000,  ""temperature"": 0} ".Replace("question", msg);
             var content = new StringContent(JsonString, Encoding.UTF8, "application/json");
             var response = client.PostAsync(uri, content).Result;
             var JSON = response.Content.ReadAsStringAsync().Result;
+
             return Newtonsoft.Json.JsonConvert.DeserializeObject<Result>(JSON);
         }
     }
@@ -40,26 +28,35 @@ namespace isRock.Template
     public class Choice
     {
         public string text { get; set; }
+
         public int index { get; set; }
+
         public object logprobs { get; set; }
+
         public string finish_reason { get; set; }
     }
 
     public class Result
     {
         public string id { get; set; }
+
         public string @object { get; set; }
+
         public int created { get; set; }
+
         public string model { get; set; }
+
         public List<Choice> choices { get; set; }
+
         public Usage usage { get; set; }
     }
 
     public class Usage
     {
         public int prompt_tokens { get; set; }
+
         public int completion_tokens { get; set; }
+
         public int total_tokens { get; set; }
     }
-
 }
--- Controllers/LineWebHookController.cs
+++ Controllers/LineWebHookController.cs
@@ -1,9 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
+using System;
+using System.Linq;
 
 namespace isRock.Template
 {
@@ -13,34 +10,49 @@ namespace isRock.Template
         [HttpPost]
         public IActionResult POST()
         {
-            var AdminUserId = "__________Line_Admin_User_ID________________";
+            var AdminUserId = "U4fb865f6dbe9b0ed361b915695022e49";
 
             try
             {
                 //設定ChannelAccessToken
-                this.ChannelAccessToken = "______________Line_Channel_Access_Token______________";
+                this.ChannelAccessToken = "6Gi6Wrb66sqTZodRKegVRzJzTp8LDkTMdVC4znBwoaGaC1SmCvLrKPbCFU+Z1LXM2feIMugYuskA/AD4Sl7DkEWkveexAQQuAYlkQxiEU9q/vvzzzehEoqY3ZnhEtK60O1u/3pACPh5sTe+E//BzzgdB04t89/1O/w1cDnyilFU=";
+
                 //配合Line Verify
-                if (ReceivedMessage.events == null || ReceivedMessage.events.Count() <= 0 ||
-                    ReceivedMessage.events.FirstOrDefault().replyToken == "00000000000000000000000000000000") return Ok();
+                if(ReceivedMessage.events == null || ReceivedMessage.events.Count() <= 0 ||
+                    ReceivedMessage.events.FirstOrDefault().replyToken == "00000000000000000000000000000000")
+                {
+                    return Ok();
+                }
+
                 //取得Line Event
                 var LineEvent = this.ReceivedMessage.events.FirstOrDefault();
                 var responseMsg = "";
+
                 //準備回覆訊息
-                if (LineEvent.type.ToLower() == "message" && LineEvent.message.type == "text")
+                if(LineEvent.type.ToLower() == "message" && LineEvent.message.type == "text")
+                {
                     responseMsg = $"{  isRock.Template.ChatGPT.CallChatGPT(LineEvent.message.text).choices.FirstOrDefault().text}";
-                else if (LineEvent.type.ToLower() == "message")
+                }
+                else if(LineEvent.type.ToLower() == "message")
+                {
                     responseMsg = $"收到 event : {LineEvent.type} type: {LineEvent.message.type} ";
+                }
                 else
+                {
                     responseMsg = $"收到 event : {LineEvent.type} ";
+                }
+
                 //回覆訊息
                 this.ReplyMessage(LineEvent.replyToken, responseMsg);
+
                 //response OK
                 return Ok();
             }
-            catch (Exception ex)
+            catch(Exception ex)
             {
                 //回覆訊息
                 this.PushMessage(AdminUserId, "發生錯誤:\n" + ex.Message);
+
                 //response OK
                 return Ok();
             }
